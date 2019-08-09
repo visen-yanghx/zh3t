9032436f17df53da2df5c7e06f89c3500a6011c2
visen-yanghx <yanghaixue9@163.com>
2019-08-09 15:21:56 +0800

    yanghx

--- src/zh3t_demo.c
+++ src/zh3t_demo.c
@@ -1,7 +1,7 @@
 
 /*
       Created on: 8,7,2019
-     Author:visrn-yanghx
+     Author:yanghx
 zh3t_demo
 write:
 bram_vptr[1][0]  SH_time         ;  //0x82000000     bram_vptr[1][12]  TIME_sec    ;  //0x82000060
